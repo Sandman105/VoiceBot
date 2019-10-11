b3b4db5377fffe46a4ea47c1b1136fa28e2caac7
Matt Lorber <matthew.lorber@outlook.com>
2019-10-11 12:40:38 -0400

    merged js youtube retrieve and embed

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -16,3 +16,6 @@ for (var i = 0; i < results.length; i++) {
     var videoDiv = $("<div>");
     videoDiv.append(`<iframe width="560" height="315" src="https://www.youtube.com/embed/${results[i].id.videoId}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`);
     $("#gifs-appear-here").prepend(videoDiv);
+}
+
+// not closed
