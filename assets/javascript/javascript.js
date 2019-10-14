5e9b92ea5a49d3565a2644f658d70dba42623e85
Matt Lorber <matthew.lorber@outlook.com>
2019-10-14 09:34:56 -0400

    added velocity.js animation

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,21 +1,32 @@
-$.ajax({
-    url: "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=" + searchTerm + "&key=AIzaSyCyd6FBuENCPG6Cky-aFkXNXCguunssJuo",
-    type: "GET",
-    data: { id: $(this).data("id") },
-    success: function (response) {
-        console.log(response);
-    },
-    error: function (jqXhr, status, error) {
-        alert(error);
-    }
-});
-
-//takes ajax call and embeds video into page
-var results = response.items
-for (var i = 0; i < results.length; i++) {
-    var videoDiv = $("<div>");
-    videoDiv.append(`<iframe width="560" height="315" src="https://www.youtube.com/embed/${results[i].id.videoId}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`);
-    $("#gifs-appear-here").prepend(videoDiv);
+function searchYouTube() {
+// calls youtube api
+    $.ajax({
+        url: "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=" + searchTerm + "&key=AIzaSyCyd6FBuENCPG6Cky-aFkXNXCguunssJuo",
+        type: "GET",
+        data: { id: $(this).data("id") },
+        success: function (response) {
+            console.log(response);
+        },
+        error: function (jqXhr, status, error) {
+            alert(error);
+        }
+    });
 }
 
-// not closed
+function responseYouTube() {
+//takes ajax call and embeds video into page
+    var results = response.items
+    for (var i = 0; i < results.length; i++) {
+        var videoDiv = $("<div>");
+        videoDiv.append(`<iframe width="560" height="315" src="https://www.youtube.com/embed/${results[i].id.videoId}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`);
+        $("#gifs-appear-here").prepend(videoDiv);
+    }
+}
+
+
+$("#speakButton").mousedown(function(){
+	$(this)
+		.velocity({ translateY: "-30px", rotateZ: "10deg" }, 100, "easeOut")
+		.velocity({ rotateZ: "-8deg" }, 150)
+		.velocity({ translateY: "0", rotateZ: "0" }, {duration: 600, easing: [ 500, 14 ]});
+});
